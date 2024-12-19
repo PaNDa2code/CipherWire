49bef0bd474cd4a27d1e231ac75e0d6e2a925c5a
PaNDa2Code <moaaz0688@gmail.com>
2024-12-19 14:35:46 +0200

    created caser_cipher and updated setup.py and project.toml

--- setup.py
+++ setup.py
@@ -1,1 +1,15 @@
+#!/bin/python3
+
 from setuptools import setup
+
+setup(
+        name="cypher_wire",
+        version = "0.0.0",
+        description="",
+        author="PaNDa2Code",
+        author_email="moaaz0688@gmail.com",
+        package_dir={"source" : "src/"},
+        url="https://github.com/PaNDa2code/CypherWire",
+)
+
+
